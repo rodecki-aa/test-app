7596cf0ce31e1202693efa7ca84925ef6c139b67
rodecki-aa <tomasz.rodecki@theaa.com>
2025-12-09 17:00:20 +0000

    Move routing from CarController annotations to routes.yaml

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -8,10 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
-#[Route('/api/cars')]
 class CarController extends AbstractController
 {
     public function __construct(
@@ -20,7 +18,6 @@ class CarController extends AbstractController
     ) {
     }
 
-    #[Route('', name: 'api_cars_create', methods: ['POST'])]
     public function create(Request $request): JsonResponse
     {
         try {
@@ -74,7 +71,6 @@ class CarController extends AbstractController
         }
     }
 
-    #[Route('', name: 'api_cars_list', methods: ['GET'])]
     public function list(Request $request): JsonResponse
     {
         try {
@@ -111,7 +107,6 @@ class CarController extends AbstractController
         }
     }
 
-    #[Route('/{id}', name: 'api_cars_get', methods: ['GET'])]
     public function get(string $id): JsonResponse
     {
         try {
@@ -133,7 +128,6 @@ class CarController extends AbstractController
         }
     }
 
-    #[Route('/{id}', name: 'api_cars_delete', methods: ['DELETE'])]
     public function delete(string $id): JsonResponse
     {
         try {
@@ -151,7 +145,6 @@ class CarController extends AbstractController
         }
     }
 
-    #[Route('/init-index', name: 'api_cars_init_index', methods: ['POST'])]
     public function initIndex(): JsonResponse
     {
         try {
